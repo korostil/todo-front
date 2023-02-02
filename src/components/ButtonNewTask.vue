150af9a48ab2c810ee5aa0c032c43ededce762b9
Alexander Korostil <av.korostil@gmail.com>
2023-02-03 00:19:44 +0300

    Compact tasks creation into dialog on "new task" button click

--- src/components/ButtonNewTask.vue
+++ src/components/ButtonNewTask.vue
@@ -1,17 +1,67 @@
 <script setup>
+import { ref } from "vue";
+import { createTask } from "@/store/api/tasks";
 import { useRouter } from "vue-router";
 
+let dialog = ref(false);
 const router = useRouter();
+let task = ref({});
+
+function doCreate() {
+  return createTask(task.value)
+    .then((json) => {
+      dialog.value = false;
+      router.push({ name: "task", params: { task_id: json.data.id } });
+    })
+    .catch(() => {
+      console.log("error");
+    });
+}
 </script>
 
 <template>
-  <v-btn
-    variant="plain"
-    prepend-icon="mdi-plus"
-    @click="router.push({ name: 'new_task' })"
-  >
-    new task
-  </v-btn>
+  <v-dialog v-model="dialog" max-width="30%" rounded="lg">
+    <template v-slot:activator="{ props }">
+      <v-btn variant="plain" prepend-icon="mdi-plus" v-bind="props">
+        new task
+      </v-btn>
+    </template>
+
+    <v-card>
+      <v-container>
+        <v-card-title>
+          <span class="text-h4">New task</span>
+        </v-card-title>
+
+        <v-container>
+          <v-text-field
+            color="grey-darken-2"
+            label="Title"
+            v-model="task.title"
+          ></v-text-field>
+          <v-text-field
+            color="grey-darken-2"
+            label="Description"
+            v-model="task.description"
+          ></v-text-field>
+          <v-checkbox label="Decisive" v-model="task.decisive"></v-checkbox>
+
+          <v-btn-toggle v-model="task.space" active-color="primary">
+            <v-label> Space </v-label>
+            <v-btn icon="mdi-account"></v-btn>
+            <v-btn icon="mdi-domain"></v-btn>
+          </v-btn-toggle>
+          <!-- TODO due field -->
+          <!-- TODO project field -->
+        </v-container>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" @click="doCreate"> Create </v-btn>
+        </v-card-actions>
+      </v-container>
+    </v-card>
+  </v-dialog>
 </template>
 
 <style scoped></style>
--- src/router/tasks.js
+++ src/router/tasks.js
@@ -5,19 +5,9 @@ export const routes = [
     component: () => import("../views/tasks/TaskListView.vue"),
   },
   {
-    path: "/tasks/new/",
-    name: "new_task",
-    component: () => import("../views/tasks/CreateTaskView.vue"),
-  },
-  {
     path: "/tasks/:task_id/",
     name: "task",
     component: () => import("../views/tasks/TaskView.vue"),
-  },
-  {
-    path: "/tasks/:task_id/update/",
-    name: "update_task",
-    component: () => import("../views/tasks/CreateTaskView.vue"),
   },
   {
     path: "/tasks/completed/",
