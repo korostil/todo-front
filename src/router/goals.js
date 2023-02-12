e53fa637d6841934977609762ad3963473d2914f
Alexander Korostil <av.korostil@gmail.com>
2023-02-12 09:56:04 +0300

    Drop CreateGoalView.vue

--- src/router/goals.js
+++ src/router/goals.js
@@ -1,10 +1,5 @@
 export const routes = [
   {
-    path: "/goals/new/",
-    name: "new_goal",
-    component: () => import("../views/goals/CreateGoalView.vue"),
-  },
-  {
     path: "/goals/",
     name: "goal_list",
     component: () => import("../views/goals/GoalListView.vue"),
@@ -13,10 +8,5 @@ export const routes = [
     path: "/goals/:goal_id/",
     name: "goal",
     component: () => import("../views/goals/GoalView.vue"),
-  },
-  {
-    path: "/goals/:goal_id/update/",
-    name: "update_goal",
-    component: () => import("../views/goals/CreateGoalView.vue"),
   },
 ];
